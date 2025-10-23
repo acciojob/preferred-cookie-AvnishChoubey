af7a6b3566e029615eaa1f34964d4c5678c11a86
Avnish Choubey <118668098+AvnishChoubey@users.noreply.github.com>
2025-10-23 20:25:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+	const fontsize = getCookie("fontsize");
+	const fontcolor = getCookie('fontcolor');
+	const root = document.documentElement;
+	root.style.setProperty('--fontsize', `${fontsize}px`);
+	root.style.setProperty('--fontcolor', `${fontcolor}`);
+});
+
+document.getElementById('submit').addEventListener('click', () => {
+	const fontSize = document.getElementById('fontsize');
+	const fontColor = document.getElementById('fontcolor');
+	document.cookie = `fontSize=${fontSize.value}`;
+	document.cookie = `fontColor=${fontColor.value}`;
+});
